dae81e590fd3ed51ae1d7393ba084a8c2a0ce19a
Ian <devnull@localhost>
2013-11-04 23:22:54 -0800

    + Implemented basic CUDA call!

--- FractalRenderer/CudaFractalRender.cu
+++ FractalRenderer/CudaFractalRender.cu
@@ -1,17 +1,34 @@
 
 #include "CudaFractalRender.cuh"
 
+#include "cuda_runtime.h"
+
+
+__global__ void Kernel(u8 * Image, u32 const Width, u32 const Height)
+{
+	u32 x = (blockIdx.x) * blockDim.x + threadIdx.x;
+	u32 y = (blockIdx.y) * blockDim.y + threadIdx.y;
+
+	if (x >= Width || y >= Height)
+		return;
+	
+	Image[y * Width * 3 + x * 3 + 0] = (u8) (x / (f32) Width * 255.f);
+	Image[y * Width * 3 + x * 3 + 1] = (u8) (x / (f32) Width * 255.f);
+	Image[y * Width * 3 + x * 3 + 2] = 0;
+}
 
 u8 const * CudaRenderFractal(u32 const Width, u32 const Height)
 {
-	u8 * Image = new u8[Width * Height * 3];
+	u8 * HostImage = new u8[Width * Height * 3];
+	u8 * DeviceImage; cudaMalloc((void**) & DeviceImage, Width * Height * 3 * sizeof(u8));
+	
+	u32 const BlockSize = 16;
+	dim3 const Grid(
+		Width / BlockSize + (Width % BlockSize ? 1 : 0), 
+		Height / BlockSize + (Height % BlockSize ? 1 : 0));
+	dim3 const Block(BlockSize, BlockSize);
+	Kernel<<<Grid, Block>>>(DeviceImage, Width, Height);
 
-	for (u32 x = 0; x < Width; ++ x)
-	for (u32 y = 0; y < Height; ++ y)
-	{
-		Image[y * Width * 3 + x * 3 + 0] = 0;
-		Image[y * Width * 3 + x * 3 + 1] = (u8) (x / (f32) Width * 255.f);
-		Image[y * Width * 3 + x * 3 + 2] = (u8) (x / (f32) Width * 255.f);
-	}
-	return Image;
+	cudaMemcpy(HostImage, DeviceImage, Width * Height * 3 * sizeof(u8), cudaMemcpyDeviceToHost);
+	return HostImage;
 }

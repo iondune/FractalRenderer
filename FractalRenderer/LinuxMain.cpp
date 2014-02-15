b612d21e4119f99ce52a8857821f2f57dfab8427
Ian Dunn <iondune@gmail.com>
2014-02-14 22:08:00 -0800

    + Added Linux main version (draws single frame successfully)

---
+++ FractalRenderer/LinuxMain.cpp
@@ -0,0 +1,51 @@
+
+#include <sstream>
+#include <iomanip>
+#include <ionCore/ionTypes.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+#include "CudaFractalRenderer.cuh"
+
+
+void FlipImage(u8 * Image, u32 const X, u32 const Y)
+{
+	for (u32 j = 0; j < Y / 2; ++ j)
+	for (u32 i = 0; i < X; ++ i)
+	for (u32 c = 0; c < 3; ++ c)
+	{
+		std::swap(Image[i * 3 + j * X * 3 + c], Image[i * 3 + (Y - 1 - j) * X * 3 + c]);
+	}
+}
+
+
+int main(int argc, char * argv[])
+{
+	u32 const ScreenSizeX = 1600, ScreenSizeY = 900;
+
+	CudaFractalRenderer Renderer;
+	Renderer.Init(cvec2u(ScreenSizeX, ScreenSizeY));
+	Renderer.Params.Stride = 3;
+
+	void * DeviceBuffer;
+	u32 const BufferSize = ScreenSizeX * ScreenSizeY * sizeof(u8) * 3;
+	CheckedCudaCall(cudaMalloc((void**) & DeviceBuffer, BufferSize));
+	CheckedCudaCall(cudaMemset(DeviceBuffer, 0, BufferSize));
+	Renderer.Render(DeviceBuffer);
+
+	u8 * Copy = new u8[BufferSize];
+	CheckedCudaCall(cudaMemcpy(Copy, DeviceBuffer, BufferSize, cudaMemcpyDeviceToHost), "MemCpy");
+	CheckedCudaCall(cudaFree(DeviceBuffer), "Free");
+
+	std::stringstream FileName;
+	FileName << "OutputImages";
+	FileName << std::setw(5) << std::setfill('0') << /*CurrentDumpFrame ++*/ 0;
+	FileName << ".png";
+
+	FlipImage(Copy, ScreenSizeX, ScreenSizeY);
+	stbi_write_png(FileName.str().c_str(), ScreenSizeX, ScreenSizeY, 3, Copy, ScreenSizeX * 3);
+	delete [] Copy;
+
+	return 0;
+}
